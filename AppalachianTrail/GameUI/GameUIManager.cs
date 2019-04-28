4f2a39a973c4c1e2fca8136d5c49e7f16c3c4109
Derrick Minor <codersyntaxdev@gmail.com>
2019-04-28 19:23:58 -0400

    introducing a GameUIManager
    added a game ui manager to help manage ui resources and a level of encapsulation

---
+++ AppalachianTrail/GameUI/GameUIManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace GameUI
+{
+    public class GameUIManager
+    {
+        public GameUIManager()
+        {
+            StartGame();
+        }
+
+        public void StartGame()
+        {
+            SetGameTitle();
+            Intro();
+            GetName();
+        }
+
+        private void SetGameTitle()
+        {
+            Console.Title = GameUIConstants.ConsoleTitle;
+        }
+
+        private void Intro()
+        {
+            DisplayToUser("Hello traveler");
+            Delay();
+            DisplayToUser("And welcome to...");
+            Delay();
+            DisplayToUser(GameUIConstants.Title);
+            Delay();
+            Console.WriteLine("You're about to embark on a 2,190 mile journey stretching from Maine to Georgia");
+            Delay();
+            Console.WriteLine("You've got a long road ahead so let's get started");
+            Delay();
+        }
+
+        private string GetName()
+        {
+            Console.Write("What's your name? ");
+            return Console.ReadLine();
+        }
+
+        private void DisplayToUser(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private void Delay(int time = 2000)
+        {
+            Thread.Sleep(time);
+        }
+    }
+}
